3918f03326925f2b95dc63d24be38865fc149c88
Alex Nichol <unixpickle@gmail.com>
2014-09-27 10:18:43 -0400

    added failure guard

--- build.dart
+++ build.dart
@@ -1,8 +1,10 @@
 import 'package:anbuild/anbuild.dart';
 
 void main(_, port) {
-  var result = new TargetResult();
-  result.addIncludes('c++', ['include']);
-  result.addScanSources(['src']);
-  port.send(result.pack());
+  runFailureGuard(port, () {
+    var result = new TargetResult();
+    result.addIncludes('c++', ['include']);
+    result.addScanSources(['src']);
+    port.send(result.pack());
+  });
 }
